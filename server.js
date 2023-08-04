f3395b55b54d7cf90bc461cc41a5ae8232ddaa96
DavidGots <gotsiridzedato@yahoo.com>
2023-08-04 18:29:58 +0400

    second commit

--- server.js
+++ server.js
@@ -3,48 +3,12 @@ const port = 5000;
 
 const app = express();
 
-const ideas = [
-  {
-    id: 1,
-    text: "Positive NewsLetter, a newsletter that only shares positive, uplifting news",
-    tag: "technology",
-    username: "TonyStark",
-    date: "2022-01-02",
-  },
-  {
-    id: 2,
-    text: "Milk cartons that turn a different color the older that your milk is getting",
-    tag: "Inventions",
-    username: "SteveRogers",
-    date: "2022-01-02",
-  },
-  {
-    id: 3,
-    text: "ATM location app which lets you know where the closest ATM is and if it is in service",
-    tag: "Software",
-    username: "BruceBanner",
-    date: "2022-01-02",
-  },
-];
-
 app.get("/", (req, res) => {
   res.send({ message: "Welcome to the RandomIdeas API" });
 });
 
-// Get all ideas
-app.get("/api/ideas", (req, res) => {
-  res.send({ success: true, data: ideas });
-});
-
-app.get("/api/ideas/:id", (req, res) => {
-  const idea = ideas.find((idea) => idea.id === +req.params.id);
-
-  if (!idea) {
-    return res.status(404).json({ succes: false, error: "Resource not found" });
-  }
-
-  res.json({ success: true, data: idea });
-});
+const ideasRouter = require("./routs/ideas");
+app.use("/api/ideas", ideasRouter);
 
 app.listen(port, () => console.log(`Server Listening on Port ${port}`));
 
